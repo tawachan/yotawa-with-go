6a41c24c1580518d7a09d807417483289018316d
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-09-30 14:25:40 +0900

    Refactor codes (Divide into different files)

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
@@ -19,63 +16,4 @@ func main() {
 	http.HandleFunc("/callback", callback)
 
 	http.ListenAndServe(":"+port, nil)
-
-}
-
-func root(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Your App is now runninng !!!")
-}
-
-func callback(w http.ResponseWriter, req *http.Request) {
-	channelSecret := os.Getenv("channelSecret")
-	channelAccessToken := os.Getenv("channelAccessToken")
-
-	bot, err := linebot.New(channelSecret, channelAccessToken)
-	checkError(err)
-
-	events, err := bot.ParseRequest(req)
-	checkError(err)
-
-	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			w.WriteHeader(400)
-		} else {
-			w.WriteHeader(500)
-		}
-		return
-	}
-	// Set up slices for reply
-	for _, event := range events {
-		var replyTexts []string
-		var replyMessages []linebot.Message
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				replyTexts = getAutoResponses(message.Text)
-				if len(replyTexts) == 0 {
-					replyTexts = append(replyTexts, message.Text)
-				}
-
-			case *linebot.ImageMessage:
-				replyTexts = append(replyTexts, "image included")
-			}
-		}
-
-		// Set line structs, using slices set up above
-		for _, m := range replyTexts {
-			replyMessages = append(replyMessages, linebot.NewTextMessage(m))
-		}
-
-		// execute message-reply
-		if _, err = bot.ReplyMessage(event.ReplyToken, replyMessages...).Do(); err != nil {
-			log.Print(err)
-		}
-
-	}
-}
-
-func checkError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
 }
--- reply.go
+++ reply.go
@@ -2,26 +2,66 @@ package main
 
 import (
 	"strings"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 type dict map[string]string
+type content struct {
+	Ctype   string
+	Content string
+}
 
 var dictionary dict
 
 func init() {
 	dictionary = dict{
-		"おおたわ":  "おおたわです",
-		"OTYM":  "おたやま",
-		"test":  "test desu",
-		"コンパイル": "難しい",
+		"おおたわ":   "おおたわです",
+		"OTYM":   "おたやま",
+		"test":   "test desu",
+		"コンパイル":  "難しい",
+		"tumblr": "こちらを見てね→ https://yotawa9929.tumblr.com/",
 	}
 }
 
-func getAutoResponses(s string) []string {
-	var replyMessages []string
+func getReplyContents(event *linebot.Event) (replyContents []linebot.Message) {
+	var contents []content
+	switch message := event.Message.(type) {
+	case *linebot.TextMessage:
+		contents = getAutoResponses(message.Text)
+		if len(contents) == 0 {
+			contents = append(contents, content{"text", message.Text})
+		}
+
+	case *linebot.ImageMessage:
+		contents = append(contents, content{"text", "image detected"})
+	}
+
+	replyContents = convertToLineFormat(contents)
+	return
+}
+
+func convertToLineFormat(contents []content) (replyContents []linebot.Message) {
+	// Set up contents for line format
+	for _, c := range contents {
+		var rc linebot.Message
+		if c.Ctype == "text" {
+			rc = linebot.NewTextMessage(c.Content)
+		} else if c.Ctype == "image" {
+			rc = linebot.NewImageMessage(c.Content, c.Content)
+		} else {
+			continue
+		}
+		replyContents = append(replyContents, rc)
+	}
+	return replyContents
+}
+
+func getAutoResponses(s string) []content {
+	var replyMessages []content
 	for key, value := range dictionary {
 		if strings.Contains(s, key) {
-			replyMessages = append(replyMessages, value)
+			replyMessages = append(replyMessages, content{"text", value})
 		}
 	}
 	return replyMessages
