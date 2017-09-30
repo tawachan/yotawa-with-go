81e95b1209549b19552ed3ea56264e5bf7e18fed
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-09-30 15:31:36 +0900

    Update test for template

--- reply.go
+++ reply.go
@@ -48,7 +48,7 @@ func convertToLineFormat(contents []content) (replyContents []linebot.Message) {
 			desc := "Check this out!"
 			image := "https://68.media.tumblr.com/7433692cabbfa132f34adb034e7909fa/tumblr_inline_owu4b4v7ow1v9napg_500.png"
 
-			action := linebot.NewURITemplateAction("View", link).
+			action := linebot.NewURITemplateAction("View", link)
 			carousel := linebot.NewCarouselColumn(image, title, desc, action)
 			template := linebot.NewCarouselTemplate(carousel)
 
