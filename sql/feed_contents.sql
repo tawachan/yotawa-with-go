d7deb4a85fa988736f7d28a5a617e0b31b7549b1
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-10-02 16:22:34 +0900

    Update sql

--- sql/feed_contents.sql
+++ sql/feed_contents.sql
@@ -3,4 +3,7 @@ INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'OTYM', 
 INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'おたやま', 'OTYMはどこからともなく生まれた俗称です。そんな名前の会社が有るとかないとか。',  '', '');
 INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'ヲタヤマ', 'OTYMはどこからともなく生まれた俗称です。そんな名前の会社が有るとかないとか。',  '', '');
 INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'オタヤマ', 'OTYMはどこからともなく生まれた俗称です。そんな名前の会社が有るとかないとか。',  '', '');
-INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('link', 'Tumblr', 'Y.otawa の Tumblr',  'https://68.media.tumblr.com/avatar_1a796a63c4f8_96.png', 'https://yotawa9929.tumblr.com/');
+INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('link', 'Tumblr', 'Y.otawa の Tumblr',  'https://s3-ap-northeast-1.amazonaws.com/yotawaapp/uploads/image/image/70/414d3088-78d4-4532-9ded-1643e64fd718.png', 'https://yotawa9929.tumblr.com/');
+
+
+
