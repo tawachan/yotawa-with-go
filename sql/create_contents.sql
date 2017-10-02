539d5d216bc7f8a9f70e783e80fd5a67825f2518
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-10-02 14:54:24 +0900

    Add sql files

---
+++ sql/create_contents.sql
@@ -0,0 +1,9 @@
+CREATE TABLE contents (
+  id 			 serial,
+	Category varchar(255),
+	Key 		 varchar(255),
+	Text     varchar(255),
+	Image    varchar(255),
+	Link     varchar(255)
+);
+
---
+++ sql/feed_contents.sql
@@ -0,0 +1,5 @@
+INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'おおたわ', 'こんにちは、僕の名前は大多和です。',  '', '');
+INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'OTYM', 'OTYMはどこからともなく生まれた俗称です。そんな名前の会社が有るとかないとか。',  '', '');
+INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'おたやま', 'OTYMはどこからともなく生まれた俗称です。そんな名前の会社が有るとかないとか。',  '', '');
+INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'ヲタヤマ', 'OTYMはどこからともなく生まれた俗称です。そんな名前の会社が有るとかないとか。',  '', '');
+INSERT INTO contents (Category, Key, Text, Image, Link) VALUES ('text', 'オタヤマ', 'OTYMはどこからともなく生まれた俗称です。そんな名前の会社が有るとかないとか。',  '', '');
