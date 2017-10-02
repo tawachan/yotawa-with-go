be64095f1f9126c08c60949dd702a3d40bf6b103
Yusuke Otawa <otawa.yusuke@gmail.com>
2017-10-02 13:43:38 +0900

    Change to postgres

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/yusuke9929/yotawa-with-go/controllers"
 )
 
 func main() {
+	db, err := sql.Open("postgres", "user=postgres dbname=yotawa-with-go sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
 
+	rows, err := db.Query("SELECT * FROM users where id = 1")
+	fmt.Println(rows.Scan())
+
+	var userid int
+	err = db.QueryRow(`INSERT INTO users(name, age)
+		VALUES('beatrice', 93) RETURNING id`).Scan(&userid)
+
+	fmt.Println(err)
+	fmt.Println(userid)
 	lc := controllers.NewLineController(getSession())
 
 	port := os.Getenv("PORT")
